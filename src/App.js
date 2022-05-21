83c5cf26597e3d566ee31ecb8a687349ef2091cf
Jie Chen <jiechenmc@gmail.com>
2022-05-20 23:19:34 -0400

    removed unused useEffect from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import Map from "./components/Map";
 import Result from "./components/Result";
 import useStatesData from "./hooks/useStatesData";
